60452a5f86568eaaf3991297017dcf1206dffa69
dhrp <t.peskens@nerdalize.com>
2018-06-19 16:51:00 +0200

    update adapterconf

--- azure/setup.sh
+++ azure/setup.sh
@@ -16,7 +16,7 @@ sudo docker volume create traefik-acme-data
 sudo docker stack deploy -c docker-compose.traefik.yml traefik
 
 ## install initial adapters
-wget https://raw.githubusercontent.com/unchainio/install/master/azure/docker-compose.traefik.yml
+wget https://raw.githubusercontent.com/unchainio/install/master/azure/docker-compose.adapters.yml
 sudo docker stack deploy -c docker-compose.adapters.yml adapters
 
-echo "all done, you should now be able to access the adapter under <host>:80/apache"
+echo "all done, you should now be able to access the adapter under <host>:80/adapter"
