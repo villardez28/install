6cf26b0819043ab52165766f2f7f2d9e246110ee
dhrp <t.peskens@nerdalize.com>
2018-06-19 16:29:52 +0200

    added installation scripts for azure

---
+++ azure/setup.sh
@@ -0,0 +1,16 @@
+!#/bin/bash
+
+## Install docker
+curl -L https://get.docker.com/ |sh
+sudo usermod -aG docker ghuser
+
+## initialize swarm
+sudo docker swarm init
+
+## install traefik
+wget https://raw.githubusercontent.com/unchainio/install/master/azure/docker-compose.traefik.yml
+wget https://raw.githubusercontent.com/unchainio/install/master/azure/traefik.toml
+
+sudo docker network create -d overlay proxy
+sudo docker volume create traefik-acme-data
+sudo docker stack deploy -c docker-compose.traefik.yml traefik
